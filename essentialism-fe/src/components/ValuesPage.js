dca76f24f3667dd97064e5aaef95d8cd21c1c00b
david <davidealvarez1@gmail.com>
2020-03-04 12:45:33 -0800

    map over fetched data to create buttons

--- essentialism-fe/src/components/ValuesPage.js
+++ essentialism-fe/src/components/ValuesPage.js
@@ -1,99 +1,37 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import {Form, FormGroup, Label, Input, Button} from "reactstrap";
+import {axiosWithAuth} from "../store/axiosWithAuth";
+
 const ValuesPage = () => {
-   
+
     const [values, setValuesData] = useState([]);
     useEffect(() => {
-        axios.get("https://essentialism3.herokuapp.com/api/users/")
+        axiosWithAuth()
+        .get("/values/")
           .then(response => {
             console.log("response.data: ", response.data);
-            setValuesData(response.data.results);
+            setValuesData(response.data);
           })
           .catch(error =>{
             console.log("Data was not returned,", error)
           })
       },[])
+      console.log("values", values);
     return (
         <div className="valuesDiv">
-            <Form >
-                <FormGroup>
-                    <Label>
-                        <Button color="info">Athletics</Button> 
-                    </Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label>
-                        <Button color="info"> Art and literature</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label>
-                        <Button color="info"> Creativity, discovering, or inventing things to make a difference in the world</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label>
-                        <Button color="info"> Independence</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label >
-                        <Button color="info"> Kindness and generosity</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Living in the moment</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Membership in a social group (such as your community, racial group, or school club)</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Music</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button  color="info"> My community</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> My moral principles</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Nature and the environment</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Relationships with friends and family</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Sense of humor</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Button color="info"> Success in my career</Button>
-                    </Label>
-                </FormGroup>
-                <FormGroup >
-                    <Label >
-                        <Input type="textarea" placeholder="other: "/>
-                    </Label>
-                </FormGroup>
-                <Button>Submit</Button>
-            </Form>
+            
+            {values.map(v =>{
+                return(
+                    <Form>
+                        <FormGroup>
+                            <Label>
+                                <Button key={v.id}>{v.name}</Button>
+                            </Label>
+                        </FormGroup>
+                    </Form>
+                ) 
+            })}
         </div>
     )
    
